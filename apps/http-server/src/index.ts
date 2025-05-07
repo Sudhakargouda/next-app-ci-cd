e133ec25f19aa49bb32ee11c0e57d779bd96d016
sudhakargouda <sudhakarpatil727@gmail.com>
2025-05-07 10:32:46 +0000

    first-commit

---
+++ apps/http-server/src/index.ts
@@ -0,0 +1,29 @@
+import express from 'express'
+import {client} from '@repo/db/client'
+
+const app = express()
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send('hi there')
+})
+
+app.post('/signup', async (req, res) => {
+    const {username, password} = req.body 
+
+    const user = await client.user.create({
+        data: {
+            username,
+            password
+        }
+    })
+
+    res.json({
+        message: 'Signp successful',
+        id: user.id
+    })
+    
+})
+
+app.listen(3002)
